c670ad552bd1ef6edf6321fb719cdfc6f9d60a97
sylee <maestro30232@gmail.com>
2022-06-02 00:22:27 +0900

    update 06.01

---
+++ Nth_Alice/Assets/Script/HouseGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseGenerator : MonoBehaviour
+{
+    public GameObject housePrefab;
+    float span = 2.0f;
+    float delta = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(housePrefab) as GameObject;
+            int px = Random.Range(-30, -7);
+            go.transform.position = new Vector3(1.72f, px, 0);
+        }
+    }
+}
--- Nth_Alice/Assets/Script/PlayerController.cs
+++ Nth_Alice/Assets/Script/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 
     float distance = 10;
     void OnMouseDrag() {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Screen.height/2, distance); 
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Screen.height/1.5f, distance); 
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); 
         transform.position = objPosition;
     }
