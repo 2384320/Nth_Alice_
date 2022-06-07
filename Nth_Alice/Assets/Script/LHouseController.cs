24a034b5e67c7b95f0233c3e41d99d52e71de206
wjddms20 <102206193+wjddms20@users.noreply.github.com>
2022-06-07 16:26:59 +0900

    Update 4.26

--- Nth_Alice/Assets/Script/LHouseController.cs
+++ Nth_Alice/Assets/Script/LHouseController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LHouseController : MonoBehaviour
 {
+    GameObject alice;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.alice = GameObject.Find("alice");
     }
 
     // Update is called once per frame
@@ -19,5 +20,22 @@ public class LHouseController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.alice.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 2.0f;
+        float r2 = 0.21f;
+
+        if (d < r1 + r2)
+        {
+
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            Destroy(gameObject);
+        }
     }
+
 }
